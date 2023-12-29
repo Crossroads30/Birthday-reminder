305f73759acdd355a625d3b40b3046c7dd9da923
crossroads30 <crossroads30@mail.ru>
2023-12-29 15:03:51 +0300

    feat: add mapping for data to show

--- setup/src/List.js
+++ setup/src/List.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from 'react'
+import { data } from './data'
 
 const List = () => {
-  return (
-    <>
-      <h2>list component</h2>
-    </>
-  );
-};
+	return (
+		<>
+			<ul>
+				{data.map(person => {
+					const { id, name, age, image } = person
+					return (
+						<li key={id} className='person'>
+							<img src={image} alt='' />
+							<div>
+								<h4>{name}</h4>
+								<p>{age} years</p>
+							</div>
+						</li>
+					)
+				})}
+			</ul>
+		</>
+	)
+}
 
-export default List;
+export default List
